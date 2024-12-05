aea08a954ba46e7b0f90ac08ad528de0b694267e
Jokin Berridi <ikdgg@plaiaundi.net>
2024-12-05 18:21:41 +0100

    primerCommitJokin

--- src/App.java
+++ src/App.java
@@ -4,5 +4,6 @@ public class App {
         System.out.println("Añadiendo código de prueba");
         System.out.println("Añadiendo código desde la rama Arkaitz");
         System.out.println("Merge de las dos ramas");
+        System.out.println("En este momento Jokin al fin consiguió utilizar el puto proyecto. Aleluya!!");
     }
 }
