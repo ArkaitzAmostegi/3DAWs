252fddec59fbc1d62adb91466930e0f193766508
Jokin Berridi <ikdgg@plaiaundi.net>
2024-12-05 21:26:16 +0100

    Commit ramaJokin Arrays

--- src/App.java
+++ src/App.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Añadiendo código de prueba");
-        System.out.println("Añadiendo código desde la rama Arkaitz");
-        System.out.println("Merge de las dos ramas");
-        System.out.println("Hola buenas");
-        System.out.println("Aqui estoy modificando el codigo en ramaJokin");
+        Scanner sc= new Scanner(System.in);
+       System.out.println("Contabiliza cuantas veces sale cada número en el arrayRandom en un nuevo arrayContador:");
         mostrarArray();
+
+
+        sc.close();
     }
     public static void mostrarArray(){
         int arrayRandom[]=new int[10];
         System.out.println("Array Random:");
         for(int i=0;i<arrayRandom.length;i++){
-            arrayRandom[i]=(int)(Math.random()*(100+1-1)+1);
+            arrayRandom[i]=(int)(Math.random()*(20+1-1)+1);
             System.out.print(arrayRandom[i]+" ");
+        }
+    }
+    public static void arrayContador(int [] arrayRandom){
+        int arrayContador[]=new int [20];
+        for(int num:arrayRandom){
+            arrayContador[num-1]++;
+        }
+        for(int i=0;i<arrayContador.length;i++){
+            System.out.println("El número "+(i+1)+"aparece "+arrayContador[i]+" veces.");
         }
     }
 }
