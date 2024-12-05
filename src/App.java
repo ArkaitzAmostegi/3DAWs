e27e71c30de96a056ab36b3c87eafd379a1f64e5
Jokin Berridi <ikdgg@plaiaundi.net>
2024-12-05 20:30:27 +0100

    commitRamaJokin

--- src/App.java
+++ src/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("Añadiendo código desde la rama Arkaitz");
         System.out.println("Merge de las dos ramas");
         System.out.println("Hola buenas");
+        System.out.println("Aqui estoy modificando el codigo en ramaJokin");
     }
 }
